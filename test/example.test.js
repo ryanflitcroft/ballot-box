e3b483d241467bc38410bb1e5edcf899c108aa70
Ryan Flitcroft <90467280+ryanflitcroft@users.noreply.github.com>
2021-12-17 14:06:37 -0800

    Revert "failing test"

--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,18 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
-import { renderPoll } from '../render-utils.js';
+
 const test = QUnit.test;
 
-test('renderPoll should return a DOM element', (expect) => {
+test('time to test a function', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = '<div></div>';
+    const expected = true;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = renderPoll;
+    const actual = true;
 
     //Expect
     // Make assertions about what is expected versus the actual result
-    expect.equal(actual.outerHTML, expected);
+    expect.equal(actual, expected);
 });

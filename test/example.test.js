50673b539af5b341cc7c1c7e02b014aa44b8747c
Kyra Christensen <christensen.kyral@gmail.com>
2021-12-17 10:08:56 -0800

    failing test

--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,18 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
-
+import { renderPoll } from '../render-utils.js';
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('renderPoll should return a DOM element', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const expected = '<div></div>';
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const actual = renderPoll;
 
     //Expect
     // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
+    expect.equal(actual.outerHTML, expected);
 });
